32e836836ffd1133eb9c358526cb16f28509b904
YurijM <y-myagkov@mail.ru>
2025-04-29 11:00:24 +0300

    Изменена цветовая схема.

--- app/src/main/java/com/mu/fishermannotes/ui/theme/Color.kt
+++ app/src/main/java/com/mu/fishermannotes/ui/theme/Color.kt
@@ -2,15 +2,15 @@ package com.mu.fishermannotes.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Color0 = Color(0xFFE1F5FE)
-val Color1 = Color(0xFFB3E5FC)
-val Color2 = Color(0xFF81D4FA)
-val Color3 = Color(0xFF4FC3F7)
-val Color4 = Color(0xFF29B6F6)
-val Color5 = Color(0xFF03A9F4)
-val Color6 = Color(0xFF039BE5)
-val Color7 = Color(0xFF0288D1)
-val Color8 = Color(0xFF00838F)
-val Color9 = Color(0xFF006064)
+val Color1 = Color(0xFFBBDEFB) //val Color1 = Color(0xFFB3E5FC)
+val Color0 = Color(0xFFE3F2FD) //val Color0 = Color(0xFFE1F5FE)
+val Color2 = Color(0xFF90CAF9) //val Color2 = Color(0xFF81D4FA)
+val Color3 = Color(0xFF64B5F6) //val Color3 = Color(0xFF4FC3F7)
+val Color4 = Color(0xFF42A5F5) //val Color4 = Color(0xFF29B6F6)
+val Color5 = Color(0xFF2196F3) //val Color5 = Color(0xFF03A9F4)
+val Color6 = Color(0xFF1E88E5) //val Color6 = Color(0xFF039BE5)
+val Color7 = Color(0xFF1976D2) //val Color7 = Color(0xFF0288D1)
+val Color8 = Color(0xFF1565C0) //val Color8 = Color(0xFF00838F)
+val Color9 = Color(0xFF0D47A1) //val Color9 = Color(0xFF006064)
 
 val ColorError = Color(0xFFFF0000)
