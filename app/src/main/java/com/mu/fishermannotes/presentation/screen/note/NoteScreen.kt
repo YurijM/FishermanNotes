972f117ef6cb5d646b334c7802b7a8cf4478853a
YurijM <y-myagkov@mail.ru>
2025-04-30 15:06:37 +0300

    Тестирование функциоала загрузки фотографии.

--- app/src/main/java/com/mu/fishermannotes/presentation/screen/note/NoteScreen.kt
+++ app/src/main/java/com/mu/fishermannotes/presentation/screen/note/NoteScreen.kt
@@ -1,5 +1,10 @@
 package com.mu.fishermannotes.presentation.screen.note
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -7,21 +12,44 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberAsyncImagePainter
 import com.mu.fishermannotes.presentation.component.OkAndCancel
+import com.mu.fishermannotes.presentation.utils.toLog
 
 @Composable
 fun NoteScreen(
     viewModel: NoteViewModel = hiltViewModel()
 ) {
+    //var imageUri by remember { mutableStateOf<Uri?>(null) }
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri ->
+        if (uri != null) {
+            imageUri = uri
+            toLog("uri: $uri")
+            //onSelect(imageUri!!)
+        }
+    }
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
@@ -38,9 +66,29 @@ fun NoteScreen(
                 .padding(vertical = 12.dp)
         ) {
             Column(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .height(IntrinsicSize.Min)
             ) {
+                if (imageUri != null) {
+                    Image(
+                        painter = rememberAsyncImagePainter(model = imageUri),
+                        contentDescription = null,
+                        modifier = Modifier
+                            //.requiredSize(dimensionResource(id = R.dimen.profile_photo_size))
+                            .requiredSize(100.dp)
+                            .clip(RoundedCornerShape(8.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+                Text(
+                    text = "Выбрать фото",
+                    textDecoration = TextDecoration.Underline,
+                    modifier = Modifier.clickable {
+                        launcher.launch("image/*")
+                    },
+                )
                 HorizontalDivider(
                     thickness = 1.dp,
                     modifier = Modifier.padding(top = 8.dp)
--- app/src/main/java/com/mu/fishermannotes/presentation/utils/Constants.kt
+++ app/src/main/java/com/mu/fishermannotes/presentation/utils/Constants.kt
@@ -1,4 +1,4 @@
 package com.mu.fishermannotes.presentation.utils
 
-const val DEBUG_TAG = "fishermen"
+const val DEBUG_TAG = "fisherman"
 const val YEAR_START = 2025
